0fa775efd20942ba30fece9c74cf7f301e3294de
kervis vasquez <kervisvasquez24@gmail.com>
2020-03-20 13:02:26 -0400

    pintando localstorage en el dom

--- eventos/formulario/assets/js/app.js
+++ eventos/formulario/assets/js/app.js
@@ -14,6 +14,8 @@ function eventListener(){
     // borrar tweets
 
     listTweets.addEventListener('click', borrarTweet)
+        
+    document.addEventListener('DOMContentLoaded',localStorageListo )
     
 
 }
@@ -26,21 +28,6 @@ function agregarTweet(e)
     e.preventDefault()
     // leer el valor de textarea
     const tweet = document.getElementById('tweet').value
-    // crear boton para eliminar
-    const botonDeleted = document.createElement('a')
-    botonDeleted.classList = 'borrar-tweet'
-    botonDeleted.innerText = 'X'
-    
-    // crear li 
-    const li = document.createElement('li')
-    li.innerText = tweet
-    // añade el boton de borrar al tweet 
-    li.appendChild(botonDeleted)
-
-    // añade el tweet a la lista 
-    listTweets.appendChild(li)
-
-
     // añadir al localstorage
         agregarTweetLocalStorage(tweet)
     
@@ -57,6 +44,27 @@ function agregarTweet(e)
             alert('tweet eliminado')
         }
         
+    }
+    //mostarr datos de localstorage
+    function localStorageListo()
+    {
+        let tweets
+        tweets = obtenerTweetLocalStorage() 
+        tweets.forEach(function(tweet){
+            // crear boton para eliminar
+    const botonDeleted = document.createElement('a')
+    botonDeleted.classList = 'borrar-tweet'
+    botonDeleted.innerText = 'X'
+    
+    // crear li 
+    const li = document.createElement('li')
+    li.innerText = tweet
+    // añade el boton de borrar al tweet 
+    li.appendChild(botonDeleted)
+
+    // añade el tweet a la lista 
+    listTweets.appendChild(li)
+        });
     }
 
     // agrega tweet a localstorage
@@ -86,3 +94,5 @@ function agregarTweet(e)
         }
         return tweets
     }
+
+    
