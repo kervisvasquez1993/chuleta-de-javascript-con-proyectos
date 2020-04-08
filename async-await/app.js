6cafd111b522f259ac30ece5fe8e4c888a316178
kervis vasquez <kervisvasquez24@gmail.com>
2020-04-07 20:28:10 -0400

    agregamos async await

---
+++ async-await/app.js
@@ -0,0 +1,31 @@
+async function obtenerClientes()
+{
+    const clientes = new Promise((resolve, reject)=>{
+            setTimeout(function()
+            {
+                resolve(`clientes descargados.`)
+            },2000)
+    })
+    const error = false
+    if(!error)
+    {
+        const respuesta = await clientes
+        return respuesta
+    }
+    else
+    {
+        await Promise.reject(`hubo un error`)
+    }
+
+}
+
+obtenerClientes().then(function(respuesta)
+{
+    console.log(respuesta)
+}).catch(error => console.log(error))
+
+function saludar()
+{
+    return 'hola desde js'
+}
+console.log(saludar())
