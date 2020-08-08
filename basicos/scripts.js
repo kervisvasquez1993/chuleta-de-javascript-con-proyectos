28004ee7a5684b81810f02c2c25e8131f1962ba7
kervisvasquez1993 <kervisvasquez22@gmail.com>
2020-08-08 14:50:04 -0400

    agregamos metofods con valor numerico

--- basicos/scripts.js
+++ basicos/scripts.js
@@ -1,5 +1,8 @@
 
 // metodos para string
+
+/*
+
 let mensaje
 mensaje = 'entonces dije buen curso'
 mensaje = 'aprendiendo javascript, css y html para ser frontend'
@@ -11,4 +14,38 @@ console.log(mensaje.replace('css','php'))
 console.log(mensaje.includes('css'))// es como indexOf pero te regresa un false
 let tienda = `tienda `
 console.log(tienda.repeat(5))
+*/
 
+
+// Metodos para numeros 
+
+const   numero1 = 10,
+        numero2 = 20,
+        numero3 = 30.30,
+        numero4 = .1020,
+        numero5 = -3
+
+let resultado
+// suma 
+resultado = numero1 + numero2
+resultado = numero1 - numero2                          
+resultado = numero1 / numero2
+resultado = numero1 % numero2
+// pi
+resultado = Math.PI
+resultado = Math.round(2.2)
+resultado = Math.ceil(2.4)
+resultado = Math.floor(2.4)
+resultado = Math.sqrt(9) // sacar raiz cuadrada
+resultado = Math.abs(numero5) // absoluto
+resultado = Math.pow(8,3) // multiplicar a determinada potencia
+resultado = Math.min(3,6,5,1,4)
+resultado = Math.max(3,6,5,1,4)
+resultado = Math.random()
+
+let puntaje;
+puntaje = 10
+++puntaje  // asigna el valor antes de imprimirlo
+puntaje++ // asiga el valor despues de imprimirlo
+puntaje += 3
+console.log(puntaje)
