339ac8f75d41c13551ffd87a0e3ad7fde2b6496a
kervisvasquez1993 <kervisvasquez22@gmail.com>
2020-08-16 18:51:35 -0400

    agregamos metodos para recorrer un array

--- basicos/scripts.js
+++ basicos/scripts.js
@@ -1,5 +1,5 @@
 
-// metodos para string
+//=========================== metodos para string =========================
 
 /*
 
@@ -15,9 +15,9 @@ console.log(mensaje.includes('css'))// es como indexOf pero te regresa un false
 let tienda = `tienda `
 console.log(tienda.repeat(5))
 */
+/*
 
-
-// Metodos para numeros 
+//============================ Metodos para numeros ================================= 
 
 const   numero1 = 10,
         numero2 = 20,
@@ -49,3 +49,210 @@ puntaje = 10
 puntaje++ // asiga el valor despues de imprimirlo
 puntaje += 3
 console.log(puntaje)
+*/
+/*
+let tipo_dedatos;
+// valores orimitivos
+tipo_dedatos= 'kervis'
+tipo_dedatos = 20
+tipo_dedatos = 20.20
+tipo_dedatos = -30
+tipo_dedatos = "20"
+tipo_dedatos = false
+//fin de primitivo
+// valores de referencia
+tipo_dedatos = [1,2,3]
+tipo_dedatos = {
+    nombre    :   'kervis',
+    profesion : 'programador'
+}
+tipo_dedatos = new Date()
+
+console.log(typeof tipo_dedatos)
+*/
+
+
+//===================operadores de comparacion ==============================//
+/*
+
+const num1 = 20,
+      num2 = 30,
+      num3 = "20"
+
+      console.log(num1 > num2)
+      console.log(num1 < num2)
+      console.log( num1 == num3)
+      console.log( num1 === num3)
+      console.log(num1 != num3)
+      console.log('hola' == ' hola')
+      console.log('z' > 'b' )
+      console.log(null === undefined)
+
+      */
+
+
+      // =============================================  arreglo ===================================//
+
+/*
+      const meses = ['ENERO', 'FEBRERO', 'MARZO', 'ABRIR']
+      console.log(Array.isArray(meses))
+      console.log(meses)
+      meses[4] = 'MAYO'
+      meses.push('JUNIO')
+      console.log(meses)
+      //BUSCAR DATOS EN ARREGLO
+      console.log(meses.indexOf('MAYO'))
+      //AÑADIR AL INICIO DEL ARREGLO
+      meses.unshift('mes 0')
+      console.log(meses)
+
+      // eliminar un elemento del arregl
+      meses.pop()
+      console.log(meses)
+
+      //eliminar del principio
+
+      meses.shift()
+      console.log(meses)
+
+      //quitar un rango
+      meses.splice(2,1)
+      console.log(meses)
+
+      // revertir meses
+      meses.reverse()
+      console.log(meses)
+
+      const arr1 = [1,2,3,4]
+      const arr2 = [6,5,3,12,13,65,34]
+      console.log(arr1.concat(arr2))
+
+      const frutas = ['platanos', 'manzanas', 'fresas', 'naranja']
+      // ordenar arreglo cn letras
+      frutas.sort()
+      console.log(frutas)
+      //
+      arr2.sort()
+      console.log(arr2)
+      // ordenar arreglo con numeros
+
+      arr2.sort(function(a,b){
+          return a - b
+      })
+
+      console.log(arr2)
+
+      */
+
+
+      //==================================== objetos =================================//
+
+      const persona = {
+          'nombre'    : 'kervis',
+          'apellid'   : 'martinez',
+          'profesion' : 'proframador',
+          'edad'      : 26,
+          'musica'    : ['Trance','ballenat','rock'],
+          'ciudad'    : {
+              'ciudad' : 'valencia',
+              'pais'   : 'venezuela'
+          },
+          fechaCumple: function(){
+              return new Date().getFullYear() - this.edad
+          }
+      }
+
+     //arreglo de objetos
+
+     const autos = [
+         { modelo : 'carro1', motor : 'motor1' },
+         { modelo : 'carro2', motor : 'motor2' },
+         { modelo : 'carro3', motor : 'motor3' }
+     ]
+
+    autos[0].modelo = 'audi'
+    //console.log(autos)
+
+    //=============================================== funciones ==================================//
+
+    // functin declaretion 
+
+   // ================== for loop ====================
+
+   for(let i = 0 ; i < 10 ; i++){
+      // console.log(`numero: ${i}`)
+   }
+
+   const arreglo = ['camisa','pantalon','correa','gorra']
+   for(let i = 0 ; i < arreglo.length; i++){
+       console.log(arreglo[i])
+   }
+
+   // while 
+
+   let i = 0
+   while(i < 10 ){
+       console.log(`numero: ${i}`)
+       i++
+       if( i === 5 ){
+           console.log(' 5 es el numero')
+           continue
+           i++
+       }
+   }
+
+   do{
+       console.log(`numero: ${i}`)
+       i++
+   }
+   while(i < 10)
+
+   //foreach
+
+   const pendientes = ['amar a eliza', 'limpiar la casa', 'estudiar', 'dormir']
+
+   pendientes.forEach((pendiente, index) => {
+       console.log(`${index} : ${pendiente}`)
+   });
+
+   // map para recorrer un arreglo
+
+   const carrito = [
+       {id: 1 , producto : 'camisa'},
+       {id: 2 , producto : 'pantalon'},
+       {id: 3 , producto : 'gorra'},
+   ]
+
+   const nombrePrducto = carrito.map(function(carrito){
+            return `${carrito.id} : ${carrito.producto}`    
+   })
+
+   nombrePrducto.forEach( elemento =>{
+    console.log(elemento)
+   })
+
+   const automoviles = {
+       modelo : 'Camaron',
+       motor : 6.1,
+       año : 2010
+   }
+   for(let auto in automoviles){
+       console.log(`${auto} : ${automoviles[auto]}`)
+   }
+
+
+
+   // ===========================   iteradores ===============================
+
+
+   const ciudades = ['valencia', 'caracas', 'maturin']
+   const rdenes = new Set ([123,345,567,789])
+   const datos = new Map()
+   datos.set('nombre','juan')
+   datos.set('profesion', 'desarrollador web')
+
+   // entrice iterador 
+
+   for(let entradas of ciudades.entries()){
+       console.log(entradas)
+   }
