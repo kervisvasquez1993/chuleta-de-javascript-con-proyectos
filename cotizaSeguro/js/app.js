b31d6ab3385e6b960539de560f887f696cc9e4cc
kervisvasquez1993 <kervisvasquez22@gmail.com>
2020-08-23 16:32:35 -0400

    agregamos seccion cn cntructor para app se seguros

---
+++ cotizaSeguro/js/app.js
@@ -0,0 +1,110 @@
+// cotizador constructor para seguro
+function Seguro (marca,anio,tipoSeguro) {
+    this.marca = marca
+    this.anio = anio
+    this.tipoSeguro = tipoSeguro
+}
+
+Seguro.prototype.cotizarSeguro = function(informacion){
+    // marcas 
+    // 1 = americano 1.15
+    // 2 asiatico 1.05
+    // 3 = europeo 1.35
+    let cantidad
+    const base = 2000
+
+    switch(this.marca){
+        case '1' :
+            cantidad = base * 1.15
+            break
+        case '2' :
+            cantidad = base * 1.05
+            break    
+        case '3' :
+            cantidad = base * 1.35
+            break    
+    }
+
+    // leer el año 
+    const diferencia = new Date().getFullYear() - this.anio
+
+    // cantidad que se debe de pagar 
+    cantidad -= ((diferencia*3)*cantidad) / 100
+
+    // si el seguro es basico se multiplica por 30 mas 
+    // si es completo 50%
+    if(this.tipoSeguro === 'basico'){
+        cantidad *= 1.30
+    }else{
+        cantidad *= 1.50
+    }
+    return cantidad
+}
+// todo lo que se muestra
+function Interzas(){
+
+    // mensaje que se imprime en el html
+    Interzas.prototype.mostrarError = function(mensaje, tipoSeguro){
+        const div = document.createElement('div')
+        if(tipoSeguro === 'error'){
+            div.classList.add('mensaje', 'error') 
+        }else{
+            div.classList.add('mensaje', 'error')
+        }
+        div.innerHTML = `${mensaje}`
+        formulario.insertBefore(div, document.querySelector('.form-group'))
+
+        setTimeout(function(){
+                document.querySelector('.mensaje').remove()
+        }, 3000)
+    }
+}
+
+//event listener
+const formulario = document.getElementById('cotizar-seguro')
+
+formulario.addEventListener('submit', (e)=> {
+   e.preventDefault
+    // leer la marca seleccionada del select
+   const marca = document.getElementById('marca')
+   const marcaSelecionada = marca.options[marca.selectedIndex].value
+   
+   
+   //leer el año selecionado
+   const anio = document.getElementById('anio'),
+         anioSelecionado = anio.options[anio.selectedIndex].value
+         
+
+    // leer el valor del radio butom
+    const tipoRadio = document.querySelector('input[name="tipo"]:checked').value
+
+    // instacia de interfaz
+    
+    const interfaz = new Interzas()
+
+    // revisams que los campos no estan vacios
+
+    if(marcaSelecionada === '' || anioSelecionado === '' || tipoRadio === '' ){
+        interfaz.mostrarError('Faltan datos, revise el formulario y pruebe de nuevo')
+    }else{
+        const seguro = new Seguro(marcaSelecionada, anioSelecionado, tipoRadio)
+       // cotizar el seguro 
+
+       const cantidad = seguro.cotizarSeguro(seguro)
+    }
+
+})
+
+
+
+const max = new Date().getFullYear(),
+min = max -20,
+selecionarAnio = document.getElementById('anio')
+
+for(let i = max ; i >=  min; i--){
+    let option = document.createElement('option')
+    option.value = i
+    option.innerHTML = i
+    selecionarAnio.appendChild(option)
+
+}
