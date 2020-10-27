4333fb9016a2298ead7ecbce36630f6e9004b8db
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-10-27 15:58:14 -0400

    proyecto finalizado de clima con api

--- Curso JS Moderno/36-PROYECTO-Clima/js/app.js
+++ Curso JS Moderno/36-PROYECTO-Clima/js/app.js
@@ -49,6 +49,7 @@ function consultarApi(ciudad, pais)
       {
         const appID = '63b14d18fafbf0ff975b4f2d0ee0c47b';
         const url   = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`
+        Spiner()
         fetch(url)
         .then(respuesta => respuesta.json())
         .then(datos => {
@@ -71,21 +72,25 @@ function consultarApi(ciudad, pais)
 
       function mostrarDatos(datos)
       {
-          const {main: { temp,temp_max, temp_min }} = datos
+          const { name ,main: { temp,temp_max, temp_min }} = datos
           const centigrados  =  kevinAcentigrados(temp)
           const max          =  kevinAcentigrados(temp_max)
           const min          =  kevinAcentigrados(temp_min)
           const actual       =  document.createElement('p')
           actual.innerHTML   =  `${centigrados} &#8451`
           actual.classList.add('font-bold','text-6xl')
+          const nombreCiudad = document.createElement('h4')
           const resultadoDiv = document.createElement('div')
           const tempMaximo   = document.createElement('p')
           const tempMinimo   = document.createElement('p')
+          nombreCiudad.innerHTML = `${name}`
           tempMaximo.innerHTML = `Temperatura Maxima ${max} &#8451`
           tempMinimo.innerHTML = `Temperatura Minima ${min} &#8451`
           tempMaximo.classList.add('text-xl', 'text-center','text-white')
           tempMinimo.classList.add('text-xl', 'text-center','text-white')
           resultadoDiv.classList.add('text-center','text-white')
+          nombreCiudad.classList.add('text-3xl', 'text-center','text-white')
+          resultadoDiv.appendChild(nombreCiudad)
           resultadoDiv.appendChild(actual)
           resultado.appendChild(resultadoDiv)
           resultado.appendChild(tempMaximo)
@@ -100,4 +105,28 @@ function consultarApi(ciudad, pais)
           {
               resultado.removeChild(resultado.firstChild)
           }
+      }
+
+      function Spiner()
+      {
+          limpiarHTML()
+          const divSpinner = document.createElement('div')
+          divSpinner.classList.add('sk-fading-circle')
+          divSpinner.innerHTML = `
+            <div class="sk-fading-circle">
+              <div class="sk-circle1 sk-circle"></div>
+              <div class="sk-circle2 sk-circle"></div>
+              <div class="sk-circle3 sk-circle"></div>
+              <div class="sk-circle4 sk-circle"></div>
+              <div class="sk-circle5 sk-circle"></div>
+              <div class="sk-circle6 sk-circle"></div>
+              <div class="sk-circle7 sk-circle"></div>
+              <div class="sk-circle8 sk-circle"></div>
+              <div class="sk-circle9 sk-circle"></div>
+              <div class="sk-circle10 sk-circle"></div>
+              <div class="sk-circle11 sk-circle"></div>
+              <div class="sk-circle12 sk-circle"></div>
+            </div>
+          `
+          resultado.appendChild(divSpinner)
       }
