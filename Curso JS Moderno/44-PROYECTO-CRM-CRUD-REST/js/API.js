59b85bce86a7668c12ed4d94a84c232e7fe54855
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-11-24 16:36:19 -0400

    leemos informacion de archivos del json para mostrar en pantalla

--- Curso JS Moderno/44-PROYECTO-CRM-CRUD-REST/js/API.js
+++ Curso JS Moderno/44-PROYECTO-CRM-CRUD-REST/js/API.js
@@ -1,6 +1,6 @@
 const url = 'http://localhost:4001/clientes'
 export const nuevoCliente = async cliente => {
-    
+    // cuando se crea un nuevo cliente
     try{
        await fetch(url, {
             method: 'POST',
@@ -10,6 +10,19 @@ export const nuevoCliente = async cliente => {
             }
         });
         window.location.href = 'index.html';
+    }
+    catch(e)
+    {
+        console.log(e)
+    }
+}
+
+//obtiene todos los clientes
+export  const obtenerClientes = async () => {
+    try{
+        const resultado = await fetch(url);
+        const clientes = await resultado.json();
+        return clientes;
     }
     catch(e)
     {
