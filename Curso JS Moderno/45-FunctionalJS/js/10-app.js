1d9d8d6935d26bab18b568ac48413a931d4e9a29
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-11-27 16:45:46 -0400

    agregamos composition

---
+++ Curso JS Moderno/45-FunctionalJS/js/10-app.js
@@ -0,0 +1,68 @@
+const obtenerNombre = info => ({
+    mostrarNombre(){
+        console.log(`Nombre: ${info.nombre}`);
+    }
+})
+/* pasando argumento */
+const guardarEmail = info => ({
+    agregarEmail(email)
+    {
+        info.email = email
+        console.log(`Guardando Email en ${info.email}`)
+        
+    }
+});
+
+const obtenerEmail = info => ({
+    mostrarEmail(){
+        console.log(`Email: ${info.email}`);
+    }
+})
+
+function Cliente(nombre, email, empresa)
+{
+    let info = {
+        nombre,
+        email,
+        empresa,
+    }
+    return Object.assign(
+        info,
+        obtenerNombre(info),
+        guardarEmail(info),
+        obtenerEmail(info)
+        
+      
+    )
+}
+
+function Empleado(nombre, email, puesto)
+{
+    let info = 
+    {
+        nombre,
+        email,
+        puesto
+    }
+    return Object.assign(
+        info,
+        obtenerNombre(info),
+        guardarEmail(info ),
+        
+        
+    )
+
+}
+
+const cliente  = Cliente('kervis', null, 'kervis vasquez C.A.')
+cliente.mostrarNombre()
+cliente.agregarEmail('kervisvasquez@gmail.com')
+cliente.mostrarEmail();
+
+
+console.log("<=======test======>");
+const empleado = Empleado('Pedro', null, 'programador')
+empleado.mostrarNombre();
+empleado.agregarEmail('correo@correo.com') // guardar el email
+//empleado.mostraEmail();
+// esto sirve para crear funcones de uso general e ir usandola en tus objetos
